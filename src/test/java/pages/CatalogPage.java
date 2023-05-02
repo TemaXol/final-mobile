a7ed11d779ab580544208bad451d220aa679a1d7
Artem <aholevko@maxilect.com>
2023-05-02 23:47:31 +0300

    optimizations

--- src/test/java/pages/CatalogPage.java
+++ src/test/java/pages/CatalogPage.java
@@ -3,7 +3,6 @@ package pages;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$;
-import static io.appium.java_client.AppiumBy.id;
 import static org.openqa.selenium.By.xpath;
 
 public class CatalogPage {
